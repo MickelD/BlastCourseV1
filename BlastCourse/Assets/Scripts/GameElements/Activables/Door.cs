eaa14ad45ac4802988cfc54c982a0882f6248551
David I. <david.illan.lopez@gmail.com>
2024-06-25 13:35:32 +0200

    AudioFixedAndSomeOnomatopeyas

--- BlastCourse/Assets/Scripts/GameElements/Activables/Door.cs
+++ BlastCourse/Assets/Scripts/GameElements/Activables/Door.cs
@@ -9,6 +9,9 @@ public class Door : ActivableBase
     public AudioCue _openSfx;
     public AudioCue _closeSfx;
 
+    public GameObject _openVFX1;
+    public GameObject _openVFX2;
+
     private void Awake()
     {
         _animator = GetComponent<Animator>();   
@@ -21,6 +24,8 @@ public class Door : ActivableBase
         {
             AudioManager.TryPlayCueAtPoint(set ? _openSfx : _closeSfx,transform.position);
             _animator.SetBool("Open", set);
+
+            Destroy(Instantiate(Random.Range(0, 2) == 1 ? _openVFX1 : _openVFX2, transform.position + Vector3.up, Quaternion.identity), 5);
 
             if (Type != ActivableType.Action) SendAllActivations(set);
         }

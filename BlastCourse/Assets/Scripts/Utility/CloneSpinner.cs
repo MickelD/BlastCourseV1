9c950e4b25fb1d9381566cb98aabda49f83b4558
MickelD <migueldiazaceituno@gmail.com>
2024-06-25 08:40:25 +0200

    ik

--- BlastCourse/Assets/Scripts/Utility/CloneSpinner.cs
+++ BlastCourse/Assets/Scripts/Utility/CloneSpinner.cs
@@ -12,6 +12,7 @@ public class CloneSpinner : MonoBehaviour
 {
     [SerializeField] Vector2 _rotRange;
     [SerializeField] float _resetValue;
+    [SerializeField, Range(0f,1f)] float _emptyChance;
 
     [ContextMenu("Rotate Children")]
     public void RotateChildren()
@@ -76,6 +77,38 @@ public class CloneSpinner : MonoBehaviour
 
             Transform bubbles = person.parent.Find("clon_bubbles");
             if (bubbles != null) bubbles.localEulerAngles = new Vector3(bubbles.localEulerAngles.x, _resetValue, bubbles.localEulerAngles.z);
+        }
+    }
+
+
+    [ContextMenu("Try Empty All")]
+    public void TryEmptyAll()
+    {
+        foreach (MeshRenderer person in FindObjectsOfType<MeshRenderer>().Where(obj => obj.gameObject.name == "clon_character"))
+        {
+            bool show = Random.value > _emptyChance;
+
+            person.enabled = show;
+
+            Transform tubes = person.transform.parent.Find("clon_tube_character");
+            if (tubes != null && (tubes.TryGetComponent(out MeshRenderer tubeRend))) tubeRend.enabled = show;
+
+        }
+    }
+
+    [ContextMenu("Try Empty Children")]
+    public void TryEmptyChildren()
+    {
+        foreach (Transform child in transform)
+        {
+            bool show = Random.value > _emptyChance;
+
+            Transform person = child.Find("clon_character");
+            Transform tubes = child.Find("clon_tube_character");
+
+            if (person != null && person.TryGetComponent(out MeshRenderer personRend)) personRend.enabled = show;
+            if (tubes != null && tubes.TryGetComponent(out MeshRenderer tubesRend)) tubesRend.enabled = show;
+
         }
     }
 }
